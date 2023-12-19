c937386a634e1718ace65eae00d113d6ba6eccb7
zhang tao <zhang_tao@whu.edu.cn>
2023-12-19 14:22:24 +0800

    try add tap

--- llava/model/multimodal_encoder/tokenize_anything/modeling/image_tokenizer.py
+++ llava/model/multimodal_encoder/tokenize_anything/modeling/image_tokenizer.py
@@ -128,7 +128,6 @@ class ImageTokenizer(nn.Module):
         outputs["mask_pred"] = outputs["mask_pred"].gt(0)
         #outputs["mask_pred"] = outputs["mask_pred"].to(torch.uint8)
         outputs["boxes"] = batched_mask_to_box(outputs["mask_pred"])
-        print(outputs["boxes"])
 
         # flatten
         outputs_ = {}
