43e8e3deb669420d536bbccbaa1e0041674c0fcd
zhang tao <zhang_tao@whu.edu.cn>
2023-12-19 14:15:44 +0800

    try add tap

--- llava/model/multimodal_encoder/tokenize_anything/modeling/image_tokenizer.py
+++ llava/model/multimodal_encoder/tokenize_anything/modeling/image_tokenizer.py
@@ -125,8 +125,8 @@ class ImageTokenizer(nn.Module):
         # {"iou_pred" (64, 4), "mask_pred" (64, 4, h, w), "sem_tokens" (64, 4, c), "sem_embeds" (64, 4, 1024)}
 
         # perform nms
-        outputs["mask_pred"] = outputs["mask_pred"].to(torch.uint8)
         print(outputs["mask_pred"].sum(dim=-1).sum(dim=-1))
+        outputs["mask_pred"] = outputs["mask_pred"].to(torch.uint8)
         outputs["boxes"] = batched_mask_to_box(outputs["mask_pred"])
 
         # flatten
