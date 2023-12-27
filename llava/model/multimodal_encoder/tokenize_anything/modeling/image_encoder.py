dfbbe2f73621f7ddf9ab5f73751de4cfe91614a4
zhang tao <zhang_tao@whu.edu.cn>
2023-12-27 15:38:26 +0800

    try tap encoder

--- llava/model/multimodal_encoder/tokenize_anything/modeling/image_encoder.py
+++ llava/model/multimodal_encoder/tokenize_anything/modeling/image_encoder.py
@@ -234,7 +234,8 @@ class ImageEncoderViT(nn.Module):
         self.cross_indices = list(range(depth // 4 - 1, depth, depth // 4))
 
     def forward(self, x):
-        print("image_encoder_x: ", x.device)
+        if x.device != self.parameters()[0].device:
+            x = x.to(self.parameters()[0].device)
         x = self.patch_embed(x)
         x = self.pos_embed(x)
         x = space_to_depth(x, self.window_size)
