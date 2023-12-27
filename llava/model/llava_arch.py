56e1a01de8ffabaaa94eed4316e8e6dd2bc7ee0a
zhang tao <zhang_tao@whu.edu.cn>
2023-12-27 17:26:44 +0800

    try tap encoder

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -102,8 +102,7 @@ class LlavaMetaForCausalLM(ABC):
 
     def encode_images_tap_for_single_image(self, image, image_input_size, image_original_size,):
         # image (1, c, h, w)
-        print(image.shape)
-        print(image)
+        self.get_model().get_vision_tower().eval()
         image_features = self.get_model().get_vision_tower().foward_for_image_tokenize(
             image, grid_size=8, image_size=image_input_size, original_size=image_original_size,
             iou_threthold=0.8, stable_threthold=0.8, nms_threthold=0.7)['sem_embeds']  # (N, 1024)
