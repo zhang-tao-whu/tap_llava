f41e6f8a96872a885939fb518e97d484e03506a8
zhang tao <zhang_tao@whu.edu.cn>
2023-12-27 16:29:51 +0800

    try tap encoder

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -120,6 +120,9 @@ class LlavaMetaForCausalLM(ABC):
     ):
         vision_tower = self.get_vision_tower()
         print(input_ids)
+        print(past_key_values[-1][-1].shape[-2] if past_key_values is not None)
+        print(attention_mask.shape)
+        print('--------------')
         if vision_tower is None or images is None or input_ids.shape[1] == 1:
             if past_key_values is not None and vision_tower is not None and images is not None and input_ids.shape[1] == 1:
                 target_shape = past_key_values[-1][-1].shape[-2] + 1
