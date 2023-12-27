d83f1ba793cc445497aebbd9b8977b443896dd34
zhang tao <zhang_tao@whu.edu.cn>
2023-12-27 11:40:36 +0800

    try tap encoder

--- llava/serve/gradio_web_server.py
+++ llava/serve/gradio_web_server.py
@@ -240,7 +240,7 @@ def http_bot(state, model_selector, temperature, top_p, max_new_tokens, request:
     try:
         # Stream output
         response = requests.post(worker_addr + "/worker_generate_stream",
-            headers=headers, json=pload, stream=True, timeout=10)
+            headers=headers, json=pload, stream=True, timeout=1000)
         for chunk in response.iter_lines(decode_unicode=False, delimiter=b"\0"):
             if chunk:
                 data = json.loads(chunk.decode())
