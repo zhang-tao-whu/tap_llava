8ea22d03548928d6d0a50f0cf1249baea2bd8df7
zhang tao <zhang_tao@whu.edu.cn>
2023-12-28 14:16:07 +0800

    try instruction finetune

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -41,7 +41,7 @@ deepspeed llava/train/train_xformers.py \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
-    --tf32 True \
+    --tf32 False \
     --model_max_length 2048 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
