81d83d8a2f7bc13f6faaaf1fd1b771e6bfd40640
zhang tao <zhang_tao@whu.edu.cn>
2023-12-28 14:19:42 +0800

    try instruction finetune

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -29,9 +29,9 @@ deepspeed llava/train/train_xformers.py \
     --fp16 True \
     --output_dir ./checkpoints/llava-$MODEL_VERSION-finetune \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 4 \
+    --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 8 \
+    --gradient_accumulation_steps 32 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 50000 \
